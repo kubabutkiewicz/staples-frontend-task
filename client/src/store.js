aa084abfe647e64620a1cf3521698e17680a6174
Kuba Butkiewicz <jakub.butkiewicz94@gmail.com>
2019-06-10 00:39:10 +0200

    Create a store and create products list component

--- client/src/store.js
+++ client/src/store.js
@@ -5,12 +5,46 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    products: [],
+    currentPage: 1,
+    loading: false,
   },
   mutations: {
-
+    ADD_PRODUCTS(state, payload) {
+      state.products = [...payload.products];
+    },
+    LOADING(state) {
+      state.loading = !state.loading;
+    },
+    CHANGE_PAGE(state, payload) {
+      switch (payload.value) {
+        case 'next':
+          state.currentPage += 1;
+          break;
+        case 'prev':
+          state.currentPage += 1;
+          break;
+        default:
+          state.currentPage = payload.value;
+      }
+    },
   },
   actions: {
-
+    getProducts({ commit, state }) {
+      commit({
+        type: 'LOADING',
+      });
+      fetch(`http://localhost:3005/products?_page=${state.currentPage}`)
+        .then(resp => resp.json())
+        .then((prod) => {
+          commit({
+            type: 'ADD_PRODUCTS',
+            products: prod,
+          });
+          commit({
+            type: 'LOADING',
+          });
+        });
+    },
   },
 });
